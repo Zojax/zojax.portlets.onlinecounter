dddbee588b2dfcdfeb6a67c3e3f13a34c8727fe8
roman <roman.teterin@zojax.com>
2012-12-07 11:55:06 +0200

    #382 - Add logged users statistics

--- src/zojax/portlets/onlinecounter/interfaces.py
+++ src/zojax/portlets/onlinecounter/interfaces.py
@@ -1,5 +1,17 @@
 from zope import interface
+import zope.schema
+from zojax.widget.list.field import SimpleList
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory('zojax.quick.contenttypes')
 #from zojax.authentication.portlets.interfaces import _
 
 class IOnlineUsersPortlet(interface.Interface):
     """ Online users portlet """
+
+
+class IStatCounters(interface.Interface):
+    count_users_online_now = zope.schema.Int(title=u"count_users_online_now",required=False)
+    count_users_allday = SimpleList(title = _(u'count_users_allday'),required = False)
+    count_users_by_month = SimpleList(title = _(u'count_users_by_month'),required = False)
+    list_users_names = SimpleList(title = _(u'list_users_names'),required = False)
+    count_users_names = SimpleList(title = _(u'count_users_names'),required = False)
--- src/zojax/portlets/onlinecounter/online_users.py
+++ src/zojax/portlets/onlinecounter/online_users.py
@@ -1,6 +1,16 @@
 from zojax.resourcepackage.library import includeInplaceSource
 from zope.app.security.interfaces import IUnauthenticatedPrincipal
 
+from zojax.content.type.item import PersistentItem
+from zojax.content.type.container import ContentContainer
+from zojax.portlets.onlinecounter.interfaces import IStatCounters
+from zope import interface
+from zope.schema.fieldproperty import FieldProperty
+from zope.component import getUtility, queryMultiAdapter
+from zope.app.intid.interfaces import IIntIds
+from zope.app.component.hooks import getSite
+
+
 class OnlineUsersPortlet(object):
 
     def isAvailable(self):
@@ -9,3 +19,35 @@ class OnlineUsersPortlet(object):
     def render(self):
         includeInplaceSource("<script type='text/javascript'>$(document).ready(makeOnline());$('#online_number').ready(getOnlineNumber('online_number'));</script>")
         return super(OnlineUsersPortlet, self).render()
+
+    def update(self):
+        site = getSite()
+        max_online = "Max online : "
+        max_logged = "Max logged : "
+        try:
+            max_count = sorted(site['stats'].count_users_by_month, key=lambda x: x[0], reverse=True)[0]
+            self.stats_max_online = max_online + str(max_count[0])
+            stats_max_logged = list(site['stats'].count_users_names)
+            stats_max_logged.sort()
+            self.stats_max_logged = max_logged + str(stats_max_logged[-1])
+        except (IndexError,KeyError), e:
+            try:
+                max_count = sorted(site['stats'].count_users_allday, key=lambda x: x[0], reverse=True)[0]
+                self.stats_max_online = max_online + str(max_count[0])
+                stats_max_logged = len(site['stats'].list_users_names)
+                self.stats_max_logged = max_logged + str(stats_max_logged)
+            except (IndexError,KeyError), e:
+                self.stats_max_online = max_online + ' None'
+                self.stats_max_logged = max_logged + ' None'
+
+class StatCounters(PersistentItem):
+    interface.implements(IStatCounters)
+    count_users_online_now = FieldProperty(IStatCounters['count_users_online_now'])
+    count_users_allday = FieldProperty(IStatCounters['count_users_allday'])
+    count_users_by_month = FieldProperty(IStatCounters['count_users_by_month'])
+    list_users_names = FieldProperty(IStatCounters['list_users_names'])
+    count_users_names = FieldProperty(IStatCounters['count_users_names'])
+
+    @property
+    def id(self):
+        return getUtility(IIntIds).getId(self)
